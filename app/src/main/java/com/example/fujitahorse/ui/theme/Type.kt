aaa55807cc9f67a531edc60a70416a7d179619b3
kan513189-ship-it <kan513189@gmail.com>
2026-08-05 07:12:55 +0900

    add app/src/main/java/com/example/fujitahorse/ui/theme/Type.kt

---
+++ app/src/main/java/com/example/fujitahorse/ui/theme/Type.kt
@@ -0,0 +1,29 @@
+package com.example.fujitahorse.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 18.sp
+    )
+)
